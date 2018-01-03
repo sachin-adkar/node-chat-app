d2839904dc192b9304dad6e8bc71a68c7f965b65
sachinadkar <sachin.adkarr@gmail.com>
2018-01-03 13:15:59 +0530

    Emit newMessage on createMessage

--- server/server.js
+++ server/server.js
@@ -2,34 +2,30 @@ const path = require('path');
 const express = require('express');
 const http = require('http');
 
-//socket.io is used for client-server interaction
 const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
 const app = express();
 const port = process.env.PORT || 3000;
 
-var server = http.createServer(app); //equivalent to app = express()
+var server = http.createServer(app); 
 
-//we get access to server via server var, we get back socket server
-// using this we can emit or listen to the events
 var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-//registering event listner and do something when that event happens
-io.on('connection', (socket) => {  //listen for a new connection
+io.on('connection', (socket) => {  
     console.log('New user connected');
 
-//emit(create) events
-    socket.emit('newMessage', {    //sending the information as the second argument
-        from: "Sachin",
-        text: "Hey, Everything is awesome",
-        createdAt: 124
-    });
- //setting event listner to catch the request from the client
-    socket.on('createMessage', (newMessage) => {
-        console.log("New message", newMessage);
+
+    socket.on('createMessage', (message) => {
+        console.log("New message", message);
+
+        io.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        })
     });
 
 
